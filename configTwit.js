17ac3a89fde21f176cfe9a3155d059a9c2ff2bd6
carrabre <40183833+carrabre@users.noreply.github.com>
2021-03-29 16:49:12 -0700

    new keys

--- configTwit.js
+++ configTwit.js
@@ -1,8 +1,8 @@
 module.exports = {
-    consumer_key:         'n7PXD18hyoXHORoA4QKo8iJk4',
-    consumer_secret:      '8GjTb3dr70Mzrx1vGYjK2rDZ92ij9Cwl4oJWcUY17dq7tulSyU',
-    access_token:         '1372055070565494785-JHaAuZjTQciMMy5OCUud3f7MFcF4ss',
-    access_token_secret:  'sTDDNQ2Hkn4zL30Yr4OAQjPcGZrcqFMfiK5wsWgrvC9Il',
+    consumer_key:         'cqMUc8dc3VdihpJXkijXuKyDS',
+    consumer_secret:      'toKtKqM3y40cqafESEVc3HJWXFpbtbdi6gWGxf4RZDQ0f7QTtU',
+    access_token:         '1372055070565494785-vJIxVrVTkyRgVhkXN4YFLzGW07lS1U',
+    access_token_secret:  'Elo0Vn5rTBq5N4klWweFJBFi0pVCzboYV0TeYwxj2Y2BO',
     timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
     //strictSSL:            true,     // optional - requires SSL certificates to be valid.
 }
